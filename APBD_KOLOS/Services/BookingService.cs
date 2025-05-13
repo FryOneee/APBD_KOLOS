5768bcdb14d192a7e0a057c5300c0b66ad9dc0ae
FryOne <fryderykniedzwiecki@gmail.com>
2025-05-13 17:10:28 +0200

    update

--- APBD_KOLOS/Services/BookingService.cs
+++ APBD_KOLOS/Services/BookingService.cs
@@ -25,50 +25,49 @@ namespace APBD_KOLOS.Services
             infoReservationDTO bookingInfo = null;
 
             const string sql = @"
-SELECT
-    b.date       AS booking_date,
-    g.first_name                   AS guest_first,
-    g.last_name                    AS guest_last,
-    g.date_of_birth                AS guest_dob,
-    e.first_name                   AS emp_first,
-    e.last_name                    AS emp_last,
-    e.employee_number              AS emp_number,
-    a.name                         AS attraction_name,
-    a.price                        AS attraction_price,
-    ba.amount                      AS attraction_amount
+SELECT 
+    b.date,
+    g.first_name AS guest_first_name,
+    g.last_name  AS guest_last_name,
+    g.date_of_birth,
+    e.first_name AS emp_first_name,
+    e.last_name  AS emp_last_name,
+    e.employee_number,
+    a.name,
+    a.price,
+    ba.amount
 FROM Guest g
-JOIN Booking b      ON g.guest_id = b.guest_id
-JOIN Employee e     ON b.employee_id = e.employee_id
-JOIN Booking_Attraction ba ON b.booking_id = ba.booking_id
-JOIN Attraction a   ON ba.attraction_id = a.attraction_id
+INNER JOIN Booking b             ON g.guest_id = b.guest_id
+INNER JOIN Employee e            ON b.employee_id = e.employee_id
+INNER JOIN Booking_Attraction ba ON b.booking_id = ba.booking_id
+INNER JOIN Attraction a          ON a.attraction_id = ba.attraction_id
 WHERE b.booking_id = @booking_id;
 ";
 
             await using var conn = new SqlConnection(_cs);
-            await using var cmd = new SqlCommand(sql, conn);
+            await using var cmd  = new SqlCommand(sql, conn);
             cmd.Parameters.AddWithValue("@booking_id", bookingId);
-
             await conn.OpenAsync();
-            await using var reader = await cmd.ExecuteReaderAsync();
 
+            var reader = await cmd.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
                 if (bookingInfo == null)
                 {
                     bookingInfo = new infoReservationDTO
                     {
-                        date = reader.GetDateTime(reader.GetOrdinal("booking_date")),
-                        guest = new guestDTO
+                        date        = reader.GetDateTime(reader.GetOrdinal("date")),
+                        guest       = new guestDTO
                         {
-                            firstName    = reader.GetString(reader.GetOrdinal("guest_first")),
-                            lastName     = reader.GetString(reader.GetOrdinal("guest_last")),
-                            dateOfBirth  = reader.GetDateTime(reader.GetOrdinal("guest_dob"))
+                            firstName   = reader.GetString(reader.GetOrdinal("guest_first_name")),
+                            lastName    = reader.GetString(reader.GetOrdinal("guest_last_name")),
+                            dateOfBirth = reader.GetDateTime(reader.GetOrdinal("date_of_birth"))
                         },
-                        employee = new employeeDTO
+                        employee    = new employeeDTO
                         {
-                            firstName       = reader.GetString(reader.GetOrdinal("emp_first")),
-                            lastName        = reader.GetString(reader.GetOrdinal("emp_last")),
-                            employeeNumber  = reader.GetString(reader.GetOrdinal("emp_number"))
+                            firstName      = reader.GetString(reader.GetOrdinal("emp_first_name")),
+                            lastName       = reader.GetString(reader.GetOrdinal("emp_last_name")),
+                            employeeNumber = reader.GetString(reader.GetOrdinal("employee_number"))
                         },
                         attractions = new List<attractionDTO>()
                     };
@@ -76,9 +75,9 @@ WHERE b.booking_id = @booking_id;
 
                 bookingInfo.attractions.Add(new attractionDTO
                 {
-                    name   = reader.GetString(reader.GetOrdinal("attraction_name")),
-                    price  = reader.GetDecimal(reader.GetOrdinal("attraction_price")),
-                    amount = reader.GetInt32(reader.GetOrdinal("attraction_amount"))
+                    name   = reader.GetString(reader.GetOrdinal("name")),
+                    price  = reader.GetDecimal(reader.GetOrdinal("price")),
+                    amount = reader.GetInt32(reader.GetOrdinal("amount"))
                 });
             }
 
@@ -89,74 +88,81 @@ WHERE b.booking_id = @booking_id;
         {
             await using var conn = new SqlConnection(_cs);
             await conn.OpenAsync();
-            using var tx = conn.BeginTransaction();
+            await using var tx = (SqlTransaction)await conn.BeginTransactionAsync();
 
             try
             {
-                int employeeId;
-                await using (var cmdEmp = new SqlCommand(
-                    "SELECT employee_id FROM Employee WHERE employee_number = @emp_num", conn, tx))
+                using (var cmd = new SqlCommand(
+                           "SELECT COUNT(1) FROM Employee WHERE employee_number = @employee_number",
+                           conn, tx))
                 {
-                    cmdEmp.Parameters.AddWithValue("@emp_num", request.employeeNumber);
-                    var empObj = await cmdEmp.ExecuteScalarAsync();
-                    if (empObj == null)
-                        throw new KeyNotFoundException("Employee nie istnieje.");
-                    employeeId = (int)empObj;
-                }
-
-                await using (var cmdGuest = new SqlCommand(
-                    "SELECT 1 FROM Guest WHERE guest_id = @guest_id", conn, tx))
-                {
-                    cmdGuest.Parameters.AddWithValue("@guest_id", request.guestId);
-                    var exists = (int?)await cmdGuest.ExecuteScalarAsync() == 1;
+                    cmd.Parameters.AddWithValue("@employee_number", request.employeeNumber);
+                    var exists = (int)await cmd.ExecuteScalarAsync() > 0;
                     if (!exists)
-                        throw new KeyNotFoundException("Guest nie istnieje.");
+                        throw new KeyNotFoundException("Employee doesn't exist.");
                 }
 
-                await using (var cmdIns = new SqlCommand(
-                    @"INSERT INTO Booking (guest_id, employee_id, date)
-                      VALUES (@guest_id, @emp_id, @date);
-                      SELECT SCOPE_IDENTITY();", conn, tx))
+                using (var cmd = new SqlCommand(
+                           "SELECT COUNT(1) FROM Guest WHERE guest_id = @guest_id",
+                           conn, tx))
                 {
-                    cmdIns.Parameters.AddWithValue("@guest_id", request.guestId);
-                    cmdIns.Parameters.AddWithValue("@emp_id", employeeId);
-                    cmdIns.Parameters.AddWithValue("@date", DateTime.UtcNow);
-                    var newIdObj = await cmdIns.ExecuteScalarAsync();
-                    if (newIdObj == null)
-                        throw new Exception("Nie udało się dodać rezerwacji.");
-                    request.bookingId = Convert.ToInt32(newIdObj);
+                    cmd.Parameters.AddWithValue("@guest_id", request.guestId);
+                    var exists = (int)await cmd.ExecuteScalarAsync() > 0;
+                    if (!exists)
+                        throw new KeyNotFoundException("Guest doesn't exist.");
                 }
 
-                foreach (var attr in request.attractions)
+                int newBookingId;
+                using (var cmd = new SqlCommand(
+                           @"
+INSERT INTO Booking (guest_id, employee_id, date)
+VALUES (@guest_id, @employee_id, @date);
+SELECT CAST(SCOPE_IDENTITY() AS int);
+",
+                           conn, tx))
                 {
-                    int attractionId;
-                    await using (var cmdAtr = new SqlCommand(
-                        "SELECT attraction_id FROM Attraction WHERE name = @name", conn, tx))
+                    cmd.Parameters.AddWithValue("@guest_id",   request.guestId);
+                    cmd.Parameters.AddWithValue("@employee_id", request.employeeNumber);
+                    cmd.Parameters.AddWithValue("@date",        DateTime.UtcNow);
+
+                    newBookingId = Convert.ToInt32(await cmd.ExecuteScalarAsync());
+                }
+
+                foreach (var attraction in request.attractions)
+                {
+                    int attrId;
+                    using (var cmd = new SqlCommand(
+                               "SELECT attraction_id FROM Attraction WHERE name = @name",
+                               conn, tx))
                     {
-                        cmdAtr.Parameters.AddWithValue("@name", attr.name);
-                        var atrObj = await cmdAtr.ExecuteScalarAsync();
-                        if (atrObj == null)
-                            throw new KeyNotFoundException($"Atrakcja '{attr.name}' nie istnieje.");
-                        attractionId = (int)atrObj;
+                        cmd.Parameters.AddWithValue("@name", attraction.name);
+                        var result = await cmd.ExecuteScalarAsync();
+                        if (result == null)
+                            throw new KeyNotFoundException($"Attraction  doesn't exist.");
+                        attrId = (int)result;
                     }
 
-                    await using (var cmdInsBa = new SqlCommand(
-                        @"INSERT INTO Booking_Attraction (booking_id, attraction_id, amount)
-                          VALUES (@booking_id, @attraction_id, @amount);", conn, tx))
+                    using (var cmd = new SqlCommand(
+                               @"
+INSERT INTO Booking_Attraction (booking_id, attraction_id, amount)
+VALUES (@booking_id, @attraction_id, @amount);
+",
+                               conn, tx))
                     {
-                        cmdInsBa.Parameters.AddWithValue("@booking_id", request.bookingId);
-                        cmdInsBa.Parameters.AddWithValue("@attraction_id", attractionId);
-                        cmdInsBa.Parameters.AddWithValue("@amount", attr.amount);
-                        await cmdInsBa.ExecuteNonQueryAsync();
+                        cmd.Parameters.AddWithValue("@booking_id",    newBookingId);
+                        cmd.Parameters.AddWithValue("@attraction_id", attrId);
+                        cmd.Parameters.AddWithValue("@amount",        attraction.amount);
+
+                        await cmd.ExecuteNonQueryAsync();
                     }
                 }
 
-                tx.Commit();
-                return request.bookingId;
+                await tx.CommitAsync();
+                return newBookingId;
             }
             catch
             {
-                tx.Rollback();
+                await tx.RollbackAsync();
                 throw;
             }
         }
